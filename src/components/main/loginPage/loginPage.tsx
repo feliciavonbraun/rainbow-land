68b04c803bf2ffc926b71534e63d4087a429d285
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-04-20 16:11:21 +0200

    main logic and root style and ourRides

---
+++ src/components/main/loginPage/loginPage.tsx
@@ -0,0 +1,8 @@
+
+
+function LoginPage() {
+    return (
+        <div></div>
+    )
+}
+export default LoginPage; 
---
+++ src/components/main/startPage/startpage.tsx
@@ -0,0 +1,13 @@
+import Gallery from "./gallary";
+import OurRides from "./ourRides";
+
+
+function StartPage() {
+    return (
+        <div>
+            <OurRides />
+            <Gallery />
+        </div>
+    )
+}
+export default StartPage; 
