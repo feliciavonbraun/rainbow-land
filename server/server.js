82c41b44aa52a38dc2ec30784f05b9489d54c786
feliciavonbraun <feliciavonbraun@hotmail.com>
2021-04-21 16:39:09 +0200

    rest file working

--- server/server.js
+++ server/server.js
@@ -1,20 +1,19 @@
 const express = require('express');
-const mongoose = require('mongoose');
-require('express-async-errors');
-const userRouter = require('./routers/user.router');
-
-const PORT = 4000;
 const app = express();
+const port = 4000;
+
+const test = [
+    {
+        "name": "mike",
+        "age": "23"
+    }
+]
 
 app.use(express.json());
-app.get('/', (req, res) => res.send('Hello World'));
+app.get('/', (req, res) => {
+    res.send(test);
+})
 
-// app.use(userRouter);
-
-// (async function run() {
-//     await mongoose.connect('mongodb//localhost:27017/art', { useNewUrlParser: true, useUnifiedTopology: true});
-    
-//     app.listen(PORT, () => {
-//         console.log(`Server is running on port http://localhost:${PORT}`);
-//     })
-// })();
+app.listen(port, () => {
+    console.log(`Server is running on port http://localhost:${port}`);
+})
