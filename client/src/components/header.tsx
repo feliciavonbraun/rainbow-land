f8bf032daadcd78d19de99d18cdbba9b88c941fc
Jennifer Tendell <jennifer.tendell@gmail.com>
2021-04-23 20:20:14 +0200

    position logo in header.

--- client/src/components/header.tsx
+++ client/src/components/header.tsx
@@ -4,21 +4,31 @@ import { Link } from "react-router-dom";
 
 function Header() {
     return (
-        <div style={rootStyle}>
+        <header style={rootStyle}>
             <h1>Rainbow Land</h1>
-            <Link to='/loginPage/'>
+            <Link to='/loginPage/' style={loginButton}>
                 <Button
                     variant='contained'
                     color='primary'
+                    size='small'
                 >
-                    Log In
+                    Login
                 </Button>
             </Link>
-        </div>
+        </header>
     )
 }
 export default Header;
 
 const rootStyle: CSSProperties = {
+    position: 'relative',
     display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+}
+
+const loginButton: CSSProperties = {
+    position: 'absolute',
+    right: '3rem',
+    textDecoration: 'none',
 }
