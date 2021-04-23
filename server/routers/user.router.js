76e64143baa948e3c30af88d3926c7bd74d1f9dd
Mikaela Andersson <mikaelaaand@gmail.com>
2021-04-23 15:17:40 +0200

    fixed put-request logic and added some comments

--- server/routers/user.router.js
+++ server/routers/user.router.js
@@ -11,40 +11,44 @@ userRouter.get('/', async (req, res) => {
 
 // Add user if username is unique
 userRouter.post('/', async (req, res) => {
+// ny användare fyller i användarnamn och lösenord
     try {
         const user = new UserModel({
             username: req.body.username,
             password: req.body.password
         });
+// Kollar om användarnamnet är upptaget, om inte så skapa användare annars skicka 400.
         const findUser = await UserModel.findOne({ username: req.body.username});
         if (!findUser) {
             await user.save()
             res.status(200).json({status: user.username + ' ' + 'Registered'});
         } else {
-            res.status(400).json('user already exist');
+            res.status(400).json(`Username: ${user.username} already exist`);
         }
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-// Update user
-userRouter.put('/:id', (req, res) => {
-    const id = parseInt(req.params.id);
-    const index = testUsers.findIndex(testUser => testUser.id == id);
-
-    if(index === -1) {
-        res.status(404).json({'error': 'Oups... This user does not exist.'});
-    };
-
-    const updatedUser = {
-        id: parseInt(id),
-        ...req.body,
-    };
-
-    testUsers.splice(index, 1, updatedUser);
-
-    res.status(200).json(req.body);
+// Update user if user exist, otherwise send error message
+userRouter.put('/:id', async (req, res) => {
+    // Hittar rätt användare via id
+    try {
+        const { id } = req.params;
+        const user = await UserModel.findByIdAndUpdate(id, req.body);
+    // Om användaren inte finns, skriv ut 404 annars spara uppdatering
+        if (!user) {
+            res.status(404).json('User not found');
+        } else {
+            await user.save()
+            res.json({
+                old: user,
+                new: req.body
+            });
+        }  
+    } catch (err) {
+        res.status(500).json('User not found');
+    }
 });
 
 // Delete user
