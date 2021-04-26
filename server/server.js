e25e688d0cf96b30bdaffea772a1baa111f50855
Mikaela Andersson <mikaelaaand@gmail.com>
2021-04-26 18:34:39 +0200

    installed cookie session

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const postRouter = require('./routers/post.router.js');
 const userRouter = require('./routers/user.router.js');
+const cookieSession = require('cookie-session');
 const mongoose = require('mongoose');
 require('express-async-errors');
 const PORT = 4000; 
@@ -11,6 +12,13 @@ app.use(express.json())
 // All routers for the application
 app.use('/api/post', postRouter);
 app.use('/api/user', userRouter);
+app.use(cookieSession({
+    name: 'session',
+    secret: 'aV3ryS3cr3tK3y',
+    secure: false,
+    maxAge: 1000 * 60,
+    httpOnly: true
+}));
 
 
 (async function run() {
