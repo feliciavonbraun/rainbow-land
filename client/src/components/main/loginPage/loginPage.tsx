f63d6a3d0bfda2fd70794a0ffc51c6357430b7b3
Mikaela Andersson <mikaelaaand@gmail.com>
2021-04-27 17:23:12 +0200

    GET request work, look into other code later

--- client/src/components/main/loginPage/loginPage.tsx
+++ client/src/components/main/loginPage/loginPage.tsx
@@ -1,16 +1,62 @@
 import { Button, FormControl, TextField } from "@material-ui/core";
-import { CSSProperties } from "react";
+import { useState } from "react";
+import { CSSProperties, useEffect } from "react";
+import { makeRequest } from "../../../makeRequest";
+
+// interface NewUser {
+//     username: string,
+//     password: string
+// }
+
 
 function LoginPage() {
+    // Fetching all users in DB 
+    const [ user, setUser] = useState([]);
+    useEffect(() => {
+        const fetchUser = async () => {
+            const user = await makeRequest("/api/user", "GET")
+            setUser(user)
+        }
+        fetchUser();
+    }, []);
+
+
+    // // Add new user
+    // const [ newUser, setNewUser ] = useState<NewUser>({
+    //     username: '',
+    //     password: ''
+    // });
+    // useEffect(() => {
+    //     const body = {
+    //         "username": ,
+    //         "password": ''
+    //     }
+    //     const addUser = async () => {
+    //         const newUser = await makeRequest("/api/user/add", "POST", body)
+    //         setUser(newUser)
+    //     }
+    //     addUser();
+    // }, []);
+
+
+
+
+
     return (
         <FormControl style={form}>
             <TextField type='text' label="Username" required></TextField>
-            <TextField type='password' label="Password" required></TextField>
+            <TextField 
+                type='password' 
+                label="Password" required
+                //value={newUser.password}
+                //onChange={(event) => setNewUser(event.target.value, 'password')}
+                ></TextField>
             <Button
                 style={{ marginTop: '1rem' }}
                 type='submit'
                 variant='contained'
                 color='secondary'
+                onClick={() => {console.log(user)}}
             >
                 Sign In
             </Button>
@@ -23,3 +69,4 @@ const form: CSSProperties = {
     width: '60%',
     padding: '1rem',
 }
+
