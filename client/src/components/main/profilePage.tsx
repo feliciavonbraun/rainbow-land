1594a179a76b1b82d6a0a472cf5bc69b7b662192
Mikaela Andersson <mikaelaaand@gmail.com>
2021-04-28 22:07:27 +0200

    added logic to log out btn and go back btn

--- client/src/components/main/profilePage.tsx
+++ client/src/components/main/profilePage.tsx
@@ -1,37 +1,52 @@
-import { Button, FormControl, TextField } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import { CSSProperties } from "@material-ui/styles";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { LoginContext } from "../../loginContext";
 
 
 function ProfilePage() {
+    const { logoutUser } = useContext(LoginContext);
+
     return(
-        <main>
-            <FormControl style={form}>
-            <TextField type='text' value='' label="Username"  required></TextField>
-            <TextField type='password' value='' label="Password"  required></TextField>
+        <main style={rootStyle}>
+            <Link to='/'>
+                <Button style={goBackBtn}>Go back</Button>
+            </Link>
             <Button
                 style={{ marginTop: '1rem' }}
                 type='submit'
                 variant='contained'
                 color='secondary'
+                onClick={logoutUser}
             >
-                Update
+                Logout
             </Button>
             <Button
                 style={{ marginTop: '1rem' }}
                 type='submit'
-                variant='contained'
                 color='secondary'
             >
-                Delete
+                Delete account
             </Button>
-        </FormControl>
         </main>
     )
 }
 
-const form: CSSProperties = {
-    width: '60%',
-    padding: '1rem',
+const rootStyle: CSSProperties = {
+    height: '50vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    position: 'relative'
+
+}
+
+const goBackBtn: CSSProperties = {
+    position: 'absolute',
+    top: '2rem',
+    left: '2rem'
 }
 
 export default ProfilePage;
